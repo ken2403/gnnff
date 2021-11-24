57c2091a12a7db30d1bfb7fb3333f51f5cc42c1d
ken2403 <ken2403.pmtdg@gmail.com>
2021-11-24 12:40:43 +0900

    v0.0.3

--- src/gnnff/__version__.py
+++ src/gnnff/__version__.py
@@ -1,9 +1,12 @@
-__version_info__ = (0, 0, 2)
+__version_info__ = (0, 0, 3)
 __version__ = ".".join(map(str, __version_info__))
 
 """
 - (0.0.1)
-    edge_jkは無しで実装
+    - edge_jkは無しで実装
 - (0.0.2)
-    neighborのない箇所の距離を100に設定する(gaussian filterで値が0になり、以降の学習に使用されなくなる)
+    - neighborのない箇所の距離を100に設定する(gaussian filterで値が0になり、以降の学習に使用されなくなる)
+- (0.0.3)
+    - neighborのない箇所の距離を0に戻す(loaderとの差異をなくすため)
+    - nbr_maskをupdated_edgeに適用することで学習の際の負担を減らす
 """
