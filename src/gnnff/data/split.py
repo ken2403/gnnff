098ef32a2653d57110d3580b61d7d37fe324e242
ken2403 <ken2403.pmtdg@gmail.com>
2021-11-28 19:59:46 +0900

    adjust for existing split file

--- src/gnnff/data/split.py
+++ src/gnnff/data/split.py
@@ -6,7 +6,7 @@ from torch.utils.data import Subset
 __all__ = ["train_test_split"]
 
 
-def train_test_split(dataset, num_train, num_val, seed=None, split_path=None):
+def train_test_split(dataset, num_train, num_val, seed=0, split_path=None):
     """
     Splits the dataset into train/validation/test splits, writes split to an npz file and returns subsets.
 
@@ -14,11 +14,11 @@ def train_test_split(dataset, num_train, num_val, seed=None, split_path=None):
     ----------
     dataset : gnnff.data.CellData
         full batch of cell dataset.
-    num_train : int
+    num_train : int or None
         number of training examples
-    num_val : int
+    num_val : int or None
         number of validation examples
-    split_path : str
+    split_path : str or None
         Path to split file. Split file will be created where the generated split is stored.
 
     Returns
@@ -30,28 +30,51 @@ def train_test_split(dataset, num_train, num_val, seed=None, split_path=None):
     test : torch.utils.data.Subset
         subset with test data.
     """
-    # set random state
-    if seed is not None:
-        rs = np.random.RandomState(seed)
-    else:
-        rs = np.random.RandomState(0)
-
-    num_all_data = len(dataset)
-    if num_train + num_val > num_all_data:
-        raise ValueError(
-            "Make sure that the sum of 'num_train' and 'num_val' is smaller than the size of the data set"
-        )
-    indices = np.arange(num_all_data)
-    # shuffle indices
-    rs.shuffle(indices)
-    # get each indices
-    train_idx = indices[:num_train].tolist()
-    val_idx = indices[num_train : num_train + num_val].tolist()
-    test_idx = indices[num_train + num_val :].tolist()
-
     if split_path is not None:
         split_file = os.path.join(split_path, "split.npz")
-        np.savez(split_file, train_idx=train_idx, val_idx=val_idx, test_idx=test_idx)
+        if os.path.exists(split_file):
+            file = np.load(split_path)
+            train_idx = file["train_idx"].tolist()
+            val_idx = file["val_idx"].tolist()
+            test_idx = file["test_idx"].tolist()
+        else:
+            # set random state
+            rs = np.random.RandomState(seed)
+            num_all_data = len(dataset)
+            if num_train + num_val > num_all_data:
+                raise ValueError(
+                    "Make sure that the sum of 'num_train' and 'num_val' is smaller than the size of the data set"
+                )
+            indices = np.arange(num_all_data)
+            # shuffle indices
+            rs.shuffle(indices)
+            # get each indices
+            train_idx = indices[:num_train].tolist()
+            val_idx = indices[num_train : num_train + num_val].tolist()
+            test_idx = indices[num_train + num_val :].tolist()
+            # save split indices
+            np.savez(
+                split_file, train_idx=train_idx, val_idx=val_idx, test_idx=test_idx
+            )
+
+    if split_path is None:
+        if num_val is None or num_train is None:
+            raise ValueError("Make sure that set 'split_path' or split_num")
+        else:
+            # set random state
+            rs = np.random.RandomState(seed)
+            num_all_data = len(dataset)
+            if num_train + num_val > num_all_data:
+                raise ValueError(
+                    "Make sure that the sum of 'num_train' and 'num_val' is smaller than the size of the data set"
+                )
+            indices = np.arange(num_all_data)
+            # shuffle indices
+            rs.shuffle(indices)
+            # get each indices
+            train_idx = indices[:num_train].tolist()
+            val_idx = indices[num_train : num_train + num_val].tolist()
+            test_idx = indices[num_train + num_val :].tolist()
 
     train = Subset(dataset, train_idx)
     val = Subset(dataset, val_idx)
