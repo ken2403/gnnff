df164840095045791d54044e9458902dd07924d2
ken2403 <ken2403.pmtdg@gmail.com>
2021-11-28 19:58:38 +0900

    add count params func

--- src/gnnff/utils/script_utils.py
+++ src/gnnff/utils/script_utils.py
@@ -1,15 +1,33 @@
 import json
 from argparse import Namespace
-from logging import exception
-
-from torch._C import ScriptFunction
+import numpy as np
 
 
-__all__ = ["ScriptError", "read_from_json"]
+__all__ = ["ScriptError", "count_params", "read_from_json"]
 
 
 class ScriptError(Exception):
     pass
+
+
+def count_params(model):
+    """
+    This function takes a model as an input and returns the number of
+    trainable parameters.
+
+    Parameters
+    ----------
+    model : torch.nn.Module
+        model for which you want to count the trainable parameters.
+
+    Returns
+    -------
+    params : int
+        number of trainable parameters for the model.
+    """
+    model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+    params = sum([np.prod(p.size()) for p in model_parameters])
+    return params
 
 
 def read_from_json(jsonpath: str) -> Namespace:
