915857582b6ac7fa10f664ca9829a6cb0b190a08
ken2403 <ken2403.pmtdg@gmail.com>
2021-11-30 22:20:23 +0900

    modify docstring

--- src/gnnff/nn/activation.py
+++ src/gnnff/nn/activation.py
@@ -42,5 +42,11 @@ def shifted_softplus(x: Tensor) -> Tensor:
     -------
     torch.Tensor
         shifted soft-plus of input.
+
+    References
+    ----------
+    .. [1] Schütt, Sauceda, Kindermans, Tkatchenko, Müller:
+       SchNet - a deep learning architecture for molceules and materials.
+       The Journal of Chemical Physics 148 (24), 241722. 2018.
     """
     return F.softplus(x) - np.log(2.0)
